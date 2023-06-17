cfa450a18258931436990cdee4cda9d02909238e
Ebhota Jonathan <joe.ebh100@live.com>
2023-06-17 10:04:49 +0100

    Minor Updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,20 +5,22 @@ import Estate from "public/estate.png";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>
-        <h1 className={styles.title}>
-          Embrace Tranquil Living at our Lovely Community
-        </h1>
-        <p className={styles.desc}>
-          Welcome to Lo-Kwu Shehe Topp Community, where luxury and exclusivity
-          harmonize. Experience privacy, security, and a vibrant community
-          spirit. Discover our unparalleled amenities and embrace a truly
-          remarkable living experience.
-        </p>
-        {/* Add Button Here */}
-      </div>
-      <div className={styles.itemtwo}>
-        <Image src={Estate} alt="LSTC" className={styles.img} />
+      <div className={styles.wrapper}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>
+            Embrace Tranquil Living at our Lovely Community
+          </h1>
+          <p className={styles.desc}>
+            Welcome to Lo-Kwu Shehe Topp Community, where luxury and exclusivity
+            harmonize. Experience privacy, security, and a vibrant community
+            spirit. Discover our unparalleled amenities and embrace a truly
+            remarkable living experience.
+          </p>
+          {/* Add Button Here */}
+        </div>
+        <div className={styles.item}>
+          <Image src={Estate} alt="LSTC" className={styles.img} />
+        </div>
       </div>
     </div>
   );

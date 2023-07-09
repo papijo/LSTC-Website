45469ad25673cd9ab7be9f6fdb57657f5a5128a1
Ebhota Jonathan <joe.ebh100@live.com>
2023-07-09 22:15:32 +0100

    Update to Land Page | 09/07/23

---
+++ src/app/land/loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ComingSoon from "../../components/ComingSoon/page";
+
+const loading = () => {
+  return (
+    <div>
+      <ComingSoon />
+    </div>
+  );
+};
+
+export default loading;
--- src/app/land/page.jsx
+++ src/app/land/page.jsx
@@ -1,10 +1,151 @@
 import React from "react";
-import ComingSoon from "../../components/ComingSoon/page";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const Land = () => {
   return (
     <div>
-      <ComingSoon />
+      <h1 className={styles.title}>Available Land for Sale</h1>
+      <div className={styles.innercontainer}>
+        <div className={styles.wrapper}>
+          <div className={styles.imginfo}>
+            <img
+              src="https://images.unsplash.com/photo-1687274999959-dd29003bfa17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+              alt="Land"
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.landinfo}>
+            <h3 className={styles.landinfotitle}>Land Information</h3>
+            <div className={styles.landinnerinfo}>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Owner:</b> Ebhota Jonathan
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Plots:</b> 4
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Location:</b> Shinco Junction
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Description:</b> Square Land at 2 miles off the main road
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>C of O:</b> Yes
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>R of O:</b> Yes
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Price:</b> N45,000,000.00
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Negotiable:</b> Yes
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.wrapper}>
+          <div className={styles.imginfo}>
+            <img
+              src="https://images.unsplash.com/photo-1687274999959-dd29003bfa17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+              alt="Land"
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.landinfo}>
+            <h3 className={styles.landinfotitle}>Land Information</h3>
+            <div className={styles.landinnerinfo}>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Owner:</b> Ebhota Jonathan
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Plots:</b> 4
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Location:</b> Shinco Junction
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Description:</b> Square Land at 2 miles off the main road
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>C of O:</b> Yes
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>R of O:</b> Yes
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Price:</b> N45,000,000.00
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Negotiable:</b> Yes
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.wrapper}>
+          <div className={styles.imginfo}>
+            <img
+              src="https://images.unsplash.com/photo-1687274999959-dd29003bfa17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+              alt="Land"
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.landinfo}>
+            <h3 className={styles.landinfotitle}>Land Information</h3>
+            <div className={styles.landinnerinfo}>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Owner:</b> Ebhota Jonathan
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Plots:</b> 4
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Location:</b> Shinco Junction
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Description:</b> Square Land at 2 miles off the main road
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>C of O:</b> Yes
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>R of O:</b> Yes
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Price:</b> N45,000,000.00
+              </p>
+              <p className={styles.paragraph}>
+                {" "}
+                <b>Negotiable:</b> Yes
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
